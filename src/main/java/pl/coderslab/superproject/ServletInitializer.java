2dfa4b6d2425bf2dc888eae95b532b1dfc77b31a
Artur Hacia <artur.hacia@coderslab.pl>
2020-09-06 12:52:32 +0200

    init commit

---
+++ src/main/java/pl/coderslab/superproject/ServletInitializer.java
@@ -0,0 +1,13 @@
+package pl.coderslab.superproject;
+
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
+
+public class ServletInitializer extends SpringBootServletInitializer {
+
+    @Override
+    protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
+        return application.sources(SuperprojectApplication.class);
+    }
+
+}
---
+++ src/test/java/pl/coderslab/superproject/SuperprojectApplicationTests.java
@@ -0,0 +1,13 @@
+package pl.coderslab.superproject;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SuperprojectApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
